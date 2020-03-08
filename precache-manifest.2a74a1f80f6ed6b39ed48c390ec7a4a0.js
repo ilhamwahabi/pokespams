6d04b8513d509556f114846462dbf3fa1fd5ccd7
iwgx <wahabiputra@gmail.com>
2020-03-09 03:26:13 +0700

    Updates

---
+++ precache-manifest.2a74a1f80f6ed6b39ed48c390ec7a4a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32794981fb9f21f93f8c422957efa73",
+    "url": "/pokespams/index.html"
+  },
+  {
+    "revision": "70f542746e7080eecfc9",
+    "url": "/pokespams/static/css/main.4808c706.chunk.css"
+  },
+  {
+    "revision": "1129704e56fa3a570398",
+    "url": "/pokespams/static/js/2.9621d183.chunk.js"
+  },
+  {
+    "revision": "6d1ae0a7201d2aa71f08b67ad045a152",
+    "url": "/pokespams/static/js/2.9621d183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f542746e7080eecfc9",
+    "url": "/pokespams/static/js/main.01ae83df.chunk.js"
+  },
+  {
+    "revision": "9b4b5fe8246af4f31e7a",
+    "url": "/pokespams/static/js/runtime-main.fe96e887.js"
+  }
+]);
